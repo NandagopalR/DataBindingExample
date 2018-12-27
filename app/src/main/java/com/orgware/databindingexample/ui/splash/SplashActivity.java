6a0d060566c975c3477cdee356c697f8f62b7918
NandagopalR <gopal300491@gmail.com>
2018-12-27 17:59:38 +0530

    Some changes done

--- app/src/main/java/com/orgware/databindingexample/ui/splash/SplashActivity.java
+++ app/src/main/java/com/orgware/databindingexample/ui/splash/SplashActivity.java
@@ -1,14 +1,27 @@
 package com.orgware.databindingexample.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
+import com.orgware.databindingexample.R;
 import com.orgware.databindingexample.base.BaseActivity;
+import com.orgware.databindingexample.ui.home.HomeActivity;
 
 public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
